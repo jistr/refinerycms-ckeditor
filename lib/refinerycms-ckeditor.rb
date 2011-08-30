6e6abe33a8ea4b52f43cdbac43fa445ccbc7715f
Jiri Stransky <jiri.stransky@gmail.com>
2011-08-30 21:16:35 +0200

    whitespace fix

--- lib/refinerycms-ckeditor.rb
+++ lib/refinerycms-ckeditor.rb
@@ -3,24 +3,24 @@ require File.expand_path('../../app/helpers/ckeditor_helper', __FILE__)
 
 module Refinery
   module Ckeditor
-    
+
     class << self
       attr_accessor :root
       def root
         @root ||= Pathname.new(File.expand_path('../../', __FILE__))
       end
-      
+
       def name
         self.to_s.split("::").last
       end
-    end  
-    
+    end
+
     class Engine < ::Rails::Engine
-      
+
       initializer 'ckeditor serves assets' do |app|
         app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
       end
-            
+
       config.after_initialize do
         ::Refinery::Plugin.register do |plugin|
           plugin.name = "refinery_ckeditor"
